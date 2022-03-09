8478f6a2c354eb561ea6ea24ea4a411f7a1c0530
kgtkr <contact@kgtkr.net>
2022-03-09 16:29:56 +0900

    [WIP]dbスキーマ作成

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,5 +1,23 @@
-CREATE TABLE "user" (
-  "id" VARCHAR(32) PRIMARY KEY,
-  "icon" VARCHAR(256),
-  "password" VARCHAR(256) NOT NULL
+CREATE TABLE "users" (
+  "id" VARCHAR(64) PRIMARY KEY,
+  "created_at" TIMESTAMPTZ NOT NULL,
+  "version" INTEGER NOT NULL
 );
+
+ALTER TABLE "users" ADD INDEX "users_created_at_idx" ("created_at");
+ALTER TABLE "users" ADD INDEX "users_version_idx" ("version");
+
+CREATE TABLE "accounts" (
+  "id" VARCHAR(64) PRIMARY KEY,
+  "screen_name" VARCHAR(64) NOT NULL,
+  "name" VARCHAR(128) NOT NULL,
+  "description" VARCHAR(1024) NOT NULL,
+  "updated_at" TIMESTAMPTZ NOT NULL,
+  "icon_url" VARCHAR(1024) NOT NULL,
+  "created_at" TIMESTAMPTZ NOT NULL,
+  "version" INTEGER NOT NULL
+);
+
+ALTER TABLE "accounts" ADD INDEX "accounts_updated_at_idx" ("updated_at");
+ALTER TABLE "accounts" ADD INDEX "accounts_created_at_idx" ("created_at");
+ALTER TABLE "accounts" ADD INDEX "accounts_version_idx" ("version");
