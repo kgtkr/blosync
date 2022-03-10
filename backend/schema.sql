b577dab66c9f120ccf1b3a8f2b7ef82776292351
tkr <account@kgtkr.net>
2022-03-10 22:09:06 +0900

    fixup! [WIP]dbスキーマ作成

--- backend/schema.sql
+++ backend/schema.sql
@@ -7,17 +7,33 @@ CREATE TABLE "users" (
 ALTER TABLE "users" ADD INDEX "users_created_at_idx" ("created_at");
 ALTER TABLE "users" ADD INDEX "users_version_idx" ("version");
 
-CREATE TABLE "accounts" (
-  "id" VARCHAR(64) PRIMARY KEY,
+CREATE TABLE "account_infos" (
+  "user_id" VARCHAR(64) NOT NULL,
+  "id" VARCHAR(64) NOT NULL,
   "screen_name" VARCHAR(64) NOT NULL,
   "name" VARCHAR(128) NOT NULL,
   "description" VARCHAR(1024) NOT NULL,
   "updated_at" TIMESTAMPTZ NOT NULL,
   "icon_url" VARCHAR(1024) NOT NULL,
   "created_at" TIMESTAMPTZ NOT NULL,
-  "version" INTEGER NOT NULL
+  "version" INTEGER NOT NULL,
+  PRIMARY KEY ("user_id", "id"),
+  CONSTRAINT "account_infos_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
-ALTER TABLE "accounts" ADD INDEX "accounts_updated_at_idx" ("updated_at");
-ALTER TABLE "accounts" ADD INDEX "accounts_created_at_idx" ("created_at");
-ALTER TABLE "accounts" ADD INDEX "accounts_version_idx" ("version");
+ALTER TABLE "account_infos" ADD INDEX "account_infos_updated_at_idx" ("updated_at");
+ALTER TABLE "account_infos" ADD INDEX "account_infos_created_at_idx" ("created_at");
+ALTER TABLE "account_infos" ADD INDEX "account_infos_version_idx" ("version");
+
+CREATE TABLE "user_authed_accounts" (
+  "user_id" VARCHAR(64) NOT NULL,
+  "account_id" VARCHAR(64) NOT NULL,
+  "oauth_token" VARCHAR(1024) NOT NULL,
+  "oauth_token_secret" VARCHAR(1024) NOT NULL,
+  "created_at" TIMESTAMPTZ NOT NULL,
+  PRIMARY KEY ("user_id", "account_id"),
+  CONSTRAINT "user_authed_accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT "user_authed_accounts_account_id_ukey" UNIQUE ("account_id")
+);
+
+ALTER TABLE "user_authed_accounts" ADD INDEX "user_authed_accounts_created_at_idx" ("created_at");
